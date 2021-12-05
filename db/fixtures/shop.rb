5a49a9587c33ae725239f07cac4af9167a5cae8f
ohno <ddeeppaa.gogo@gmail.com>
2021-12-05 17:01:22 +0900

    feat: seed_fuを使いデモデータを作成 #19

---
+++ db/fixtures/shop.rb
@@ -0,0 +1,16 @@
+Shop.seed do |s|
+  s.id = 1
+  s.name = 'ラパーラ'
+  s.tel_number = '042-339-7888'
+  s.address = '東京都多摩市山王下１－１３－６'
+  s.access = '京王線・小田急線・多摩都市モノレール 多摩センター駅より徒歩7分'
+  s.open = '【ランチ】月～金曜11:30～14:00(L.O.) 【ディナー】17:00～22:00 土・日・祭日 11:30～14:00(L.O.)/17:00～22:00'
+  s.close = '毎週木曜日・第2水曜日'
+  s.member_number = '206'
+  s.member_since = '2004/06/11'
+  s.pizza_maker = ''
+  s.oven_used = ''
+  s.url = 'http://www.lapala.com/'
+  s.latitude = ''
+  s.longitude = ''
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_054309) do
+ActiveRecord::Schema.define(version: 2021_12_05_040615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,10 +39,7 @@ ActiveRecord::Schema.define(version: 2021_12_05_054309) do
     t.float "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "prefecture_id", null: false
-    t.index ["prefecture_id"], name: "index_shops_on_prefecture_id"
   end
 
   add_foreign_key "prefectures", "shops"
-  add_foreign_key "shops", "prefectures"
 end
