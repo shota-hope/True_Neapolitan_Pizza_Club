15b48c6b5d7bc11cdbf2e2838a6dd34360d3de30
ohno <ddeeppaa.gogo@gmail.com>
2021-12-11 15:36:38 +0900

    グーグルマップの表示を変更 #19

--- app/javascript/prefecture/index.js
+++ app/javascript/prefecture/index.js
@@ -22,15 +22,22 @@ window.addEventListener('load', function () {
 
 function initMap() {
   // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.036 };
+  const anpanman = { lat: 35.463, lng: 139.632 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
+    zoom: 6,
+    center: anpanman,
   });
   // The marker, positioned at Uluru
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: anpanman,
     map: map,
   });
+  const infoWindow = new google.maps.InfoWindow({ // 吹き出しの追加
+        content: '<div class="sample">アンパンマンミュージアム</div>' // 吹き出しに表示する内容
+  });
+ marker.addListener('click', function() { // マーカーをクリックしたとき
+     infoWindow.open(map, marker); // 吹き出しの表示
+    });
 }
+
