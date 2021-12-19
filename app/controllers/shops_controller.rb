5ddb0eb4b174aa42a7bbf7f1818c206c18f5eebd
ohno <ddeeppaa.gogo@gmail.com>
2021-12-19 13:14:47 +0900

    feat: shops_controller作成

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,7 @@
+class ShopsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'shops/index'
+  get 'shops/show'
   root "prefectures#index"
   resources :prefectures, only: %i[index show]
 end
