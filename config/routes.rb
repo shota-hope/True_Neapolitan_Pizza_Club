5079036532545432897fa224d38156316758acc0
ohno <ddeeppaa.gogo@gmail.com>
2021-12-21 11:55:48 +0900

    style: rubocopによる自動整形、不必要なルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'shops/index'
-  get 'shops/show'
-  root "prefectures#index"
+  root 'prefectures#index'
   resources :prefectures, only: %i[index show]
   resources :shops, only: %i[index show]
 end
