2e4c4a6c44ee266716f011ae6d0642598c235d47
ohno <ddeeppaa.gogo@gmail.com>
2021-12-28 22:31:33 +0900

    feat: グーグルマップ表示、gon導入、店舗データに緯度経度追加　#27

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'dotenv-rails'
 gem 'seed-fu'
+gem 'gon'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,9 +1,20 @@
 class ShopsController < ApplicationController
+   before_action :set_params_for_google_map, only: [:show]
+
   def index
     @shops = Shop.all
   end
 
   def show
     @shop = Shop.find(params[:id])
+  end
+
+  private
+
+  def set_params_for_google_map
+    @shop = Shop.find(params[:id])
+    gon.center_of_map_lat = @shop.latitude
+    gon.center_of_map_lng = @shop.longitude
+    gon.zoom_level_of_map = 14
   end
 end
