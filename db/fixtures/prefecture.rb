9334390293fa53912cde420983fb58f70d586b8d
ohno <ddeeppaa.gogo@gmail.com>
2021-12-30 10:16:43 +0900

    seedデータ変更

--- db/fixtures/prefecture.rb
+++ db/fixtures/prefecture.rb
@@ -1,5 +1,5 @@
 Prefecture.seed(:id,
-  { id: 1, name: '---' }, { id: 2, name: '北海道', shop_id: 1 }, { id: 3, name: '青森県' },
+  { id: 1, name: '---' }, { id: 2, name: '北海道' }, { id: 3, name: '青森県' },
   { id: 4, name: '岩手県' }, { id: 5, name: '宮城県' }, { id: 6, name: '秋田県' },
   { id: 7, name: '山形県' }, { id: 8, name: '福島県' }, { id: 9, name: '茨城県' },
   { id: 10, name: '栃木県' }, { id: 11, name: '群馬県' }, { id: 12, name: '埼玉県' },
