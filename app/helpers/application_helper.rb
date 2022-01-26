f66266cb384a03890946e117beadf719b2d1d477
ohno <ddeeppaa.gogo@gmail.com>
2022-01-26 18:02:19 +0900

    fix: メタタグ文章微調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: '真のナポリピッツァ部',
-      title: '「真のナポリピッツァ協会」認定店を簡単に探せるサービスです。',
+      title: '「真のナポリピッツァ協会」認定店を簡単に探せるサービス',
       reverse: true,
       separator: '|',
       description: '真のナポリピッツァ部は、「真のナポリピッツァ協会」認定店のみを簡単に探せるWebサービスです。お店を探してナポリピッツァを食べに行こう。',
@@ -15,8 +15,8 @@ module ApplicationHelper
       ],
       og: {
         site_name: '真のナポリピッツァ部',
-        title: '「真のナポリピッツァ協会」認定店を簡単に探せるサービスです。',
-        description: '真のナポリピッツァ部は、「真のナポリピッツァ協会」認定店のみを簡単に探せるWebサービスです。お店を探してナポリピッツァを食べに行こう。',
+        title: '「真のナポリピッツァ協会」認定店を簡単に探せるサービス',
+        description: '真のナポリピッツァ部は、「真のナポリピッツァ協会」認定店のみを簡単に探せるWebサービス。お店を探してナポリピッツァを食べに行こう。',
         type: 'website',
         url: request.original_url,
         image: image_url('biglogo.png'),
