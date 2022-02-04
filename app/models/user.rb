8457d636f0f7aa539647029d88eb81b9b5c0a151
ohno <ddeeppaa.gogo@gmail.com>
2022-02-04 11:07:47 +0900

    feat:ユーザーモデル,マイグレーション追記。db:migrate実行 #63

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :email, uniqueness: true, presence: true
+  validates :name, presence: true, length: { maximum: 255 }
 end
