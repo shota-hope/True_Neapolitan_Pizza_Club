793d9a358069686655036f9ec67007d017ed0900
ohno <ddeeppaa.gogo@gmail.com>
2022-02-04 20:30:06 +0900

    feat:　user_sessions controller作成。ログイン前ヘッダー作成 #63

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to prefectures_path
+      redirect_to root_path
     else
       render :new
     end
