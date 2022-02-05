5f387b1d75ec5c37c5327d0f52bd65fb7b81c24c
ohno <ddeeppaa.gogo@gmail.com>
2022-02-05 10:59:57 +0900

    fix: 新規登録時に自動的にログインするようコントローラ修正 #63

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       redirect_to root_path
     else
       render :new
