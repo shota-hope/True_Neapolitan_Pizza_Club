cfef655821f0ea035e36e9dfb29f2ce270d68482
ohno <ddeeppaa.gogo@gmail.com>
2022-02-06 14:28:44 +0900

    feat: 新規登録、ログイン、ヘッダーをi18n実装  #26

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module TrueNeapolitanPizzaClub
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
