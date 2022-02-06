61b08f17b708ef54d1277d153d478bd4e781a822
ohno <ddeeppaa.gogo@gmail.com>
2022-02-06 21:48:01 +0900

    feat: users/editページ作成 #66

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,13 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   private
 
   def user_params
