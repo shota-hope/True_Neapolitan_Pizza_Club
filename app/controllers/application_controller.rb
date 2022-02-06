78a8e2a2b4f8ff58c328164ab5cdaa943ff230fd
ohno <ddeeppaa.gogo@gmail.com>
2022-02-07 00:57:48 +0900

    :feat update機能を追加 # 66

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
+
+  private
+
+  def not_authenticated
+    flash[:warning] = 'ログインしてください'
+    redirect_to login_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_login, only: [:show, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -15,16 +19,32 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, success: '更新しました'
+    else
+      flash.now[:danger] = '編集に失敗しました'
+      render 'edit'
+    end
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
-  
-  private
 
   def user_params
     params.require(:user).permit(:email, :name, :profile, :avatar, :password, :password_confirmation)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
