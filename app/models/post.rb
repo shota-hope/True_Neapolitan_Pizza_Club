5a9308176be68b17900cc5c4e3cc127c55f18f8b
ohno <ddeeppaa.gogo@gmail.com>
2022-02-11 00:14:54 +0900

    feat: 店舗詳細ページ作成 #73

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,8 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 100 }
   validates :body, presence: true, length: { maximum: 10000 }
+
+  def set_date
+    created_at.strftime("%Y年%m月%d日 %R")
+  end
 end
