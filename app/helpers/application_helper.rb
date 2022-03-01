f27884f41d3232c6fb5c46d6ce14f70e8c5aaba9
ohno <ddeeppaa.gogo@gmail.com>
2022-03-02 00:08:04 +0900

    feat: サイトtitleを変更 #85

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: '真のナポリピッツァ部 TrueNapoliPizzaClub',
-      title: '真のナポリピッツァ部',
+      title: '真のナポリピッツァ部 - 真のナポリピッツァ協会認定店が探せるサービス',
       reverse: true,
       separator: '|',
       description: '真のナポリピッツァ部は、「真のナポリピッツァ協会」認定店のみを簡単に探せるWebサービスです。お店を探してナポリピッツァを食べに行こう。',
