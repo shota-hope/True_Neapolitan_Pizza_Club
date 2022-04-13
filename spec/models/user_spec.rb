256fa6340f22d5ad04035349d7095a46bff0d74b
ohno <ddeeppaa.gogo@gmail.com>
2022-04-13 18:10:16 +0900

    meta-tagの設定変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,38 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # なまえ、メール、パスワードがあれば有効な状態であること
-  it "is valid with a name, email, password, password_confirmation" do
-    user = User.new(
-      name: "ohno",
-      email: "ohno23@example.com",
-      password: "ohnoohno",
-      password_confirmation: "ohnoohno"
-      )
-    expect(user).to be_valid
-  end
+  describe "verification of validation" do
+    # なまえ、メール、パスワードがあれば有効な状態であること
+    it "is valid with a name, email, password, password_confirmation" do
+      user = User.new(
+        name: "ohno",
+        email: "ohno23@example.com",
+        password: "ohnoohno",
+        password_confirmation: "ohnoohno"
+        )
+      expect(user).to be_valid
+    end
 
-  # 名がなければ無効な状態であること
-  it "is invalid without a name" do
-    user = User.new(name: nil)
-    user.valid?
-    expect(user.errors[:name]).to include("を入力してください")
-  end
-  # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address" do
-    User.create(
-      name: "ohno",
-      email: "ohno23@example.com",
-      password: "ohnoohno",
-      password_confirmation: "ohnoohno"
+    # 名がなければ無効な状態であること
+    it "is invalid without a name" do
+      user = User.new(name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
+    # 重複したメールアドレスなら無効な状態であること
+    it "is invalid with a duplicate email address" do
+      User.create(
+        name: "ohno",
+        email: "ohno23@example.com",
+        password: "ohnoohno",
+        password_confirmation: "ohnoohno"
+        )
+      user = User.new(
+        name: "oono",
+        email: "ohno23@example.com",
+        password: "oonooono",
+        password_confirmation: "oonooono"
       )
-    user = User.new(
-      name: "oono",
-      email: "ohno23@example.com",
-      password: "oonooono",
-      password_confirmation: "oonooono"
-    )
-    user.valid?
-    expect(user.errors[:email]).to include("はすでに存在します")
+      user.valid?
+      expect(user.errors[:email]).to include("はすでに存在します")
+    end
   end
 end
