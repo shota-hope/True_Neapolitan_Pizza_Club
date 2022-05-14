17af74be240f95c4efe79e243da78e8a3b62da37
ohno <ddeeppaa.gogo@gmail.com>
2022-05-14 11:56:15 +0900

    fix: モデルスペック修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { "ohno" }
-    sequence(:email)  { |n| "tester#{n}@example.com" }
+    sequence(:email)  { |n| "hogehoge#{n}@example.com" }
     password { "ohnoohno" }
     password_confirmation { "ohnoohno" }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  # describe User do
-  #   it "有効なファクトリを持つこと" do
-  #     expect(FactoryBot.build(:user)).to be_valid
-  #   end
-  # end
-
-
-  describe "verification of validation" do
+  describe "バリデーションテスト" do
     # なまえ、メール、パスワードがあれば有効な状態であること
-    before do
-      @user = User.create(
-        name: "ohno",
-        email: "ohno23@example.com",
-        password: "ohnoohno",
-        password_confirmation: "ohnoohno"
-        )
-    end
-    
     it "is valid with a name, email, password, password_confirmation" do
-      expect(@user).to be_valid
+      user = create(:user)
+      expect(user).to be_valid
     end
 
     # 名がなければ無効な状態であること
     it "is invalid without a name" do
-      user = FactoryBot.build(:user, name: nil)
+      user = build(:user, name: nil)
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
     end
     # 重複したメールアドレスなら無効な状態であること
     it "is invalid with a duplicate email address" do
-      FactoryBot.create(:user, email: "hogehoge@example.com")
-      user = FactoryBot.build(:user, email: "hogehoge@example.com")
+      create(:user, email: "hogehoge@example.com")
+      user = build(:user, email: "hogehoge@example.com")
       user.valid?
       expect(user.errors[:email]).to include("はすでに存在します")
     end
