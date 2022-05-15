f80bd149b4dd7bc641f861abc8d3e9be7717363a
ohno <ddeeppaa.gogo@gmail.com>
2022-05-15 16:51:15 +0900

    feat:ログイン処理を成功させるテスト作成 #97

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,3 +1,5 @@
-# config.before(:each, type: :system, js: true) do
-#   driven_by :selenium_chrome_headless
-# end
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :selenium, using: :headless_chrome
+  end
+end
