a8a8d25c2a334aac8dd8602e6e8e25918fef4fdb
ohno <ddeeppaa.gogo@gmail.com>
2022-05-15 16:57:41 +0900

    feat: ログイン処理に関するテスト作成 #97

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -15,5 +15,26 @@ RSpec.describe "UserSessions", type: :system do
         expect(current_path).to eq root_path
       end
     end
+
+    context 'フォームが未入力' do
+      it 'ログイン処理が失敗する' do
+        visit login_path
+        fill_in 'メールアドレス', with: ''
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログイン'
+        expect(page).to have_content 'ログインに失敗しました'
+        expect(current_path).to eq login_path
+      end
+    end
+  end
+  describe 'ログイン後' do
+    context 'ログアウトボタンをクリック' do
+      it 'ログアウト処理が成功する' do
+        login_as(user)
+        click_link 'ログアウト'
+        expect(page).to have_content 'ログアウトしました'
+        expect(current_path).to eq root_path
+      end
+    end
   end
 end
