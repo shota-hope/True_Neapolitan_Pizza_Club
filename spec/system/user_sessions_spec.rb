cc549ac1070393b5b8535efa03d6b706a66a7024
ohno <ddeeppaa.gogo@gmail.com>
2022-05-16 10:56:29 +0900

    システムスペックのCapybaraに関する記述を削除

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,12 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
-
   let(:user) { create(:user) }
-
+  
   describe 'ログイン前' do
     context 'フォームの入力値が正常' do
       it 'ログイン処理が成功する' do
