eaf5d029e79c0e882bbe350e74661e49132e6af5
ohno <ddeeppaa.gogo@gmail.com>
2022-05-17 19:50:03 +0900

    oauthコントローラリファクタ #99

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -5,19 +5,23 @@ class OauthsController < ApplicationController
 
   def callback
     provider = auth_params[:provider]
-    if (@user = login_from(provider))
-      redirect_to root_path, notice: "#{provider.titleize}でログインしました"
-    else
-      @user = create_from(provider)
-      reset_session
-      auto_login(@user)
-      redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+    if auth_params[:denied].present?
+      redirect_to root_path, notice: 'ログインをキャンセルしました'
+      return
     end
+    create_user_from(provider) unless (@user = login_from(provider))
+    redirect_to root_path, notice: "#{provider.titleize}でログインしました"
   end
 
   private
 
   def auth_params
     params.permit(:code, :provider)
+  end
+
+  def create_user_from(provider)
+    @user = create_from(provider)
+    reset_session
+    auto_login(@user)
   end
 end
