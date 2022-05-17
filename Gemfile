0ecfa7b3b17bcd0bd16ec35c0de4e495b2ff7c69
ohno <ddeeppaa.gogo@gmail.com>
2022-05-17 14:18:40 +0900

    feat: gem configインストール

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem 'seed-fu'
 gem 'sitemap_generator'
 gem 'sorcery'
 gem 'whenever'
+gem 'config'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -117,7 +117,7 @@ Rails.application.config.sorcery.configure do |config|
   #
   config.twitter.key = ""
   config.twitter.secret = ""
-  config.twitter.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=twitter"
+  config.twitter.callback_url = "http://localhost:3000/oauth/callback?provider=twitter"
   config.twitter.user_info_mapping = {:name => "screen_name", profile: 'description', avatar: 'profile_image_url_https'}
   #
   # config.facebook.key = ""
